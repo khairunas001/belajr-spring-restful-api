720278a0b67628c56f389b28b13b5411a4148140
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-08-19 00:36:40 +0700

    19-8-2025

---
+++ src/main/java/bang_anas/restful/model/TokenResponse.java
@@ -0,0 +1,17 @@
+package bang_anas.restful.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class TokenResponse {
+
+    private String token;
+
+    private Long expiredAt;
+}
---
+++ src/main/java/bang_anas/restful/service/AuthService.java
@@ -0,0 +1,53 @@
+package bang_anas.restful.service;
+
+import bang_anas.restful.entity.User;
+import bang_anas.restful.model.LoginUserRequest;
+import bang_anas.restful.model.TokenResponse;
+import bang_anas.restful.repository.UserRepository;
+import bang_anas.restful.security.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ValidationService validationService;
+
+    @Transactional
+    public TokenResponse login(LoginUserRequest request) {
+        validationService.validate(request);
+
+        User user =
+                userRepository.findById(request.getUsername()).orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED,
+                        "username or password is wrong"
+                ));
+
+        if (BCrypt.checkpw(request.getPassword(), user.getPassword())) {
+            user.setToken(UUID.randomUUID().toString());
+            user.setTokenExpiredAt(next30Days());
+            userRepository.save(user);
+
+            return TokenResponse.builder()
+                    .token(user.getToken())
+                    .expiredAt(user.getTokenExpiredAt())
+                    .build();
+        } else {
+            throw  new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or password is wrong");
+        }
+    }
+
+    public Long next30Days() {
+        return System.currentTimeMillis() + (1000L * 60 * 12 * 30);
+    }
+
+}
